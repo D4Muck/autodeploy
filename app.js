f74394157bfd007ab24eb57c1c471a1eb0c42957
Christoph Muck <muckz11@gmail.com>
2017-04-12 12:09:33 +0200

    Implement autodeploy

---
+++ app.js
@@ -0,0 +1,33 @@
+var http = require('http');
+var childProcess = require('child_process');
+var process;
+
+var server = http.createServer(function (request, response) {
+
+    try {
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        if (process) {
+            process.kill();
+        }
+
+        process = childProcess.spawn('./deploy.sh');
+
+        process.stdout.on('data', function (chunk) {
+            response.write(chunk);
+        });
+
+        process.stderr.on('data', function (chunk) {
+            response.write('stderr:' + chunk);
+        });
+
+        process.on('close', function () {
+            response.write('closed');
+            response.end();
+        })
+    } catch (e) {
+        response.writeHead(500, {"Content-Type": "text/plain"});
+        response.write('error' + e.message);
+    }
+});
+
+server.listen(4000);
