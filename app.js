30335f4efccb7b4721f7d84929e9bee8c84d0c83
Christoph Muck <muckz11@gmail.com>
2017-04-14 11:15:17 +0200

    Change to one exec per line

--- app.js
+++ app.js
@@ -1,29 +1,41 @@
 var http = require('http');
 var childProcess = require('child_process');
+var fs = require('fs');
 var process;
 
-var server = http.createServer(function (request, response) {
+var exec = function (response, i, commands) {
+    console.log(commands[i]);
+    process = childProcess.exec(commands[i]);
 
+    process.stdout.on('data', function (chunk) {
+        response.write(chunk);
+    });
+
+    process.stderr.on('data', function (chunk) {
+        response.write('stderr:' + chunk);
+    });
+
+    process.on('close', function () {
+        if (i === commands.length - 1) {
+            response.write('closed');
+            response.end();
+        } else {
+            exec(response, ++i, commands);
+        }
+    })
+};
+
+var server = http.createServer(function (request, response) {
     try {
-        response.writeHead(200, {"Content-Type": "text/plain"});
         if (process) {
             process.kill();
         }
 
-        process = childProcess.spawn('./deploy.sh');
+        var commands = fs.readFileSync("./deploy.commands").toString().split("\n");
 
-        process.stdout.on('data', function (chunk) {
-            response.write(chunk);
-        });
+        response.writeHead(200, {"Content-Type": "text/plain"});
 
-        process.stderr.on('data', function (chunk) {
-            response.write('stderr:' + chunk);
-        });
-
-        process.on('close', function () {
-            response.write('closed');
-            response.end();
-        })
+        exec(response, 0, commands);
     } catch (e) {
         response.writeHead(500, {"Content-Type": "text/plain"});
         response.write('error' + e.message);
